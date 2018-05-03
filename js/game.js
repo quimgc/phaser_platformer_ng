b9cbc131ddd4363cdd1976192c1be3a61b05ef8b
quimgc <quimgonzalez@iesebre.com>
2018-05-03 21:50:24 +0200

    Sync

--- js/game.js
+++ js/game.js
@@ -1,21 +1,35 @@
-var level1 = {
+var playState = {
   preload: function () {
+    game.time.advancedTiming = true
+    //el preoload es carrega en memòria.
+
     //carregar les img que seran utilitzades ('nom', 'ruta/img')
     game.load.image('wall','assets/wall.png')
     game.load.image('ground','assets/ground.png')
     game.load.image('coin','assets/coin.png')
     game.load.image('enemy', 'assets/enemy.png')
+    game.load.image('dust', 'assets/dust.png')
 
     game.load.audio('coin',['assets/coin.wav', 'assets/coin.mp3'])
+    game.load.audio('dust',['assets/dust.wav', 'assets/dust.mp3'])
+    game.load.audio('jump',['assets/jump.wav', 'assets/jump.mp3'])
+    game.load.audio('dead',['assets/dead.wav', 'assets/dead.mp3'])
 
     //28 i 22 és perque la img player.png està tots els moviments de player, al haver-hi 6 skins, s'ha divit en tres columnes i s'ha partit per la mitat la plantila.
     game.load.spritesheet('player','assets/player.png', 28,22)
   },
+
   create: function () {
+    // game.physics.startSystem(Phaser.Physics.ARCADE)
     //Per moure el joc.
     this.cursor = game.input.keyboard.createCursorKeys()
 
+    //el paràmetr 0.1 és la duració del so.
     this.coinSound = game.add.audio('coin', 0.1)
+    this.dustSound = game.add.audio('dust', 0.1)
+    this.jumpSound = game.add.audio('jump', 0.1)
+    this.deadSound = game.add.audio('dead', 0.1)
+
     this.loadLevel()
 
     // el 0 és per dir quina img s'ha de carregar dels jugadors (diferents moviments durant el joc, 0,1 2, 3, 4...)
@@ -27,7 +41,19 @@ var level1 = {
     //body fa referencia al cos del jugador
     this.player.body.gravity.y = 600
 
-    this.player.animations.add('idle', [3,4,5,4], 5, true)
+    this.player.animations.add('idle',[3,4,5,4], 5, true)
+
+    this.jumping = false
+    this.playerDead = false
+
+
+    //Particules de quan salta.
+                                //posició i num max de particules
+    this.dustParticles = game.add.emitter(0,0,150)
+    this.dustParticles.makeParticles('dust')
+    this.dustParticles.setYSpeed(-200,200)
+    this.dustParticles.setXSpeed(-200,200)
+    this.dustParticles.gravity = 600
   },
   update: function () {
     //diem quins elements son amb col·lisions
@@ -35,12 +61,14 @@ var level1 = {
     game.physics.arcade.overlap(this.player, this.coins, this.takeCoin, null, this)
     game.physics.arcade.overlap(this.player, this.enemy, this.touchEnemy, null, this)
 
+    this.player.animations.play('idle')
     this.inputs()
 
-    this.player.animations.play('idle')
+
   },
   render: function () {
-
+    game.debug.text('FPS: '+game.time.fps || 'FPS: --',40,40,"#00ff00")
+    game.debug.text('jumping: '+this.jumping || 'FPS: --',40,20,"#00ff00")
   },
   loadLevel: function () {
     this.level = game.add.group()
@@ -71,16 +99,23 @@ var level1 = {
   addEnemy: function () {
     this.enemy = game.add.group()
     this.enemy.enableBody = true
-
     game.add.sprite(350, 110, 'enemy', 0, this.enemy)
   },
   takeCoin: function(a,b) {
+    b.body.enable = false
     game.add.tween(b.scale).to({x:0},150).start()
     game.add.tween(b).to({y:50},150).start()
 
     this.coinSound.play()
   },
   touchEnemy: function (a,b) {
+    if(!this.playerDead) {
+      this.playerDead = true
+      this.deadSound.play()
+      a.body.enable = false
+      game.add.tween(a.scale).to({x:0},150).start()
+      game.add.tween(a).to({y:50},150).start()
+    }
     //TODO: desactivar les fletxes per a que un cop mort, no es pugui moure.
     // window.setTimeout(function()
     // {
@@ -89,9 +124,16 @@ var level1 = {
     //   // this.player.kill()
     //
     // }, 1000);
-    this.player.kill()
+    // this.player.kill()
+  },
+
+  jumpPlayer: function () {
+    this.player.body.velocity.y = -350
+    this.jumping = true
+    this.jumpSound.play()
   },
   inputs: function () {
+    this.player.animations.play('idle')
     if (this.cursor.left.isDown) {
       //això es pot fer perque s'ha activat la fisica
       this.player.frame = 2
@@ -102,12 +144,22 @@ var level1 = {
     } else {
       this.player.body.velocity.x = 0
       this.player.frame = 0
-
     }
 
-    if (this.cursor.up.isDown) {
-      if( this.player.body.touching.down) {
-        this.player.body.velocity.y = -200
+    //quan l'usuari estigui apretant la tecla fletxa amunt i el player estigui tocant al terra -> canvio velocitat
+    if (this.player.body.touching.down) {
+
+      if(this.jumping) {
+        this.dustSound.play()
+        this.jumping = false
+        this.dustParticles.x = this.player.x + this.player.height / 2
+        this.dustParticles.y = this.player.y + this.player.height
+
+        this.dustParticles.start(true,3000,0,25)
+      }
+
+      if( this.cursor.up.isDown) {
+        this.jumpPlayer()
       }
     }
   }
@@ -115,5 +167,5 @@ var level1 = {
 
 var game = new Phaser.Game(500,200,Phaser.AUTO,'game')
 
-game.state.add('level1',level1)
-game.state.start('level1')
+game.state.add('play',playState)
+game.state.start('play')
